b9cc5c80c7e038d584375560714050a6791000a0
Leon <leon0824@gmail.com>
2025-11-09 12:34:27 +0800

    Update fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from ocpi_pydantic.v221.enum import OcpiAuthMethodEnum, OcpiReservationRestrictionTypeEnum, OcpiConnectorTypeEnum, OcpiTokenTypeEnum, OcpiWhitelistTypeEnum, OcpiConnectorFormatEnum, OcpiPowerTypeEnum, OcpiCdrDimensionTypeEnum, OcpiSessionStatusEnum, OcpiTariffDimensionTypeEnum, OcpiTariffTypeEnum, OcpiDayOfWeekEnum, OcpiStatusCodeEnum, OcpiPartyRoleEnum
+from ocpi_pydantic.v221.versions import OcpiEndpoint
 from ocpi_client.models import OcpiParty
 import pytest_asyncio
 
@@ -12,7 +13,7 @@ async def party_fixture():
         country_code='TW',
         party_id='EVO',
         party_roles=[OcpiPartyRoleEnum.EMSP, OcpiPartyRoleEnum.CPO],
-        versions_url='http://fsfs.micl.com',
+        versions_url='http://api.evo.net/ocpi/versions',
         credentials_token_for_receiving_request_from_party='aaa',
 
         credentials_token_for_sending_register_to_party='bbb',
